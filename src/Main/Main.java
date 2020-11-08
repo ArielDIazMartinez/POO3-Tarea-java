4c2c4c38fd03f1f1ae7a44eceecc86857aa17185
eliha <eliha@DESKTOP-GDNMG1Q>
2020-11-07 23:06:18 -0400

    update

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,10 +10,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		    Familia martinez = new FamiliaMartinez();	
+		    Familia Martinez = new FamiliaMartinez();	
 	        Familia sanchez = new FamiliaSanchez();
 
-	        martinez.ejecutarAccion();
+	        Martinez.ejecutarAccion();
 	       
 	        sanchez.ejecutarAccion();
 	        
